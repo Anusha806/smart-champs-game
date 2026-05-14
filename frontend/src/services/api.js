ab16b7f6421179e5ba0d7e6871308402545f676b
Anusha806 <anushamajji806@gmail.com>
2026-05-14 12:03:53 +0530

    Final frontend update1

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,24 @@
+// import axios from "axios";
+
+// const API = axios.create({
+
+//     baseURL:
+//     "https://smart-champs-backend.onrender.com/api"
+// });
+
+// export default API;
+
+
+
+
+
+
 import axios from "axios";
 
 const API = axios.create({
 
     baseURL:
-    "https://smart-champs-backend.onrender.com/api"
+    "http://localhost:5000/api"
 });
 
 export default API;
-
